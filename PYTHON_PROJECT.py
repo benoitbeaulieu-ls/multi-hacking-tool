167e566e191f33e041f51fb201f521187a5655c1
Benoit Beaulieu <benoitbeaulieu@Benoits-MBP.lan>
2020-04-13 13:07:01 -0400

    Changed all the tools to functions. Changed the menus to dictionaries. The ping option works, the port scanner works and the hydra SSH option works. Going to be completing the other funtions and add some error handling

--- PYTHON_PROJECT.py
+++ PYTHON_PROJECT.py
@@ -1,74 +1,108 @@
 import subprocess 
 from socket import *
 import time
+
+if __name__ == '__main__':
 #All menus are going to be stored here
 
-tools= ["1) gobuster",
-        "2) hydra",
-        "3) john the ripper",
-        "4) Burpsuite",
-        "5) Port Scanner",
+    tools= {
+            1: "ping",
+            2: "hydra",
+            3: "GoBuster",
+            4: "john the ripper",
+            5: "Burpsuite",
+            6: "Port Scanner",
 
-]
+        }
 
-hydra_options= ["1) SSH",
-                "2) HTTP-GET",
-]
+    hydra_options= {
+            1: "SSH",
+            2: "HTTP-GET",
 
-yes_or_no= ["1) Yes",
+    }
+    
+
+    yes_or_no= ["1) Yes",
             "2) No",
-]
+    ]
 
 
-for tool in tools:
-    print(tool)
+for number, tool in tools.items():
+    print(str(number)+" " + tool)
+
+option=int(input("what tool would you like to use?"))
 
 
-tool_select=int(input("what tool would you like to use?"))
 
-#This is go buster
-if tool_select == 1:
-    subprocess.run('ping 8.8.8.8' , shell=True)
+#All Fuctions will go here
 
-#This is Hydra
-if tool_select == 2:
-    for option in hydra_options:
-        print(option)
+# ping a host function
+def ping():
+    host = str(input("what host would you like to ping?"))
+    subprocess.run('ping %s' %(host) , shell=True)
 
-    option_select=int(input("What option would you like to use? "))
-    if option_select == 1:
-        subprocess.run("ls" , shell=True)
+# Hydra Bruteforce Function
+def hydra():
+    for number, optionselect in hydra_options.items():
+        print(str(number)+ " " + optionselect)
+    option= int(input("What are you trying to bruteforce? "))
+
+    if option == 1:
+        print("SSH Selected")
+        user= str(input("what user are you tryng to bruteforce? "))
+        wordlist= str(input("specify your wordlist path "))
+        targetIP= str(input("What is the target IP "))
+        subprocess.run('hydra -l %s -P '  %(user) + wordlist + ' ' + targetIP +' ssh -t 4' , shell=True)
+
+# GoBuster Directoy Bruteforce Function 
+def gobuster():
+    pass
+
+# John The Ripper Bruteforce Function
+def john():
+    pass
+
+# Burpsuite Open Function
+def burp():
+    pass
+
+# Stealth Port Scanner Function
+def portscanner():
+    target = input('Enter the host to be scanned: ')
+    first_Port = int(input("What port would you like to start with? "))
+    last_Port = int(input("What is the ending port?"))
+    t_IP = gethostbyname(target)
+    print ('Starting scan on host: ', t_IP)
+
+    for i in range(first_Port, last_Port):
+        s = socket(AF_INET, SOCK_STREAM)
+
+        conn = s.connect_ex((t_IP, i))
+
+        if(conn == 0):
+            print ('Port %d: OPEN' % (i,))
+
+        s.close()
+
+    print("Scan finished")
 
 
-#This is the port scanner
-if tool_select == 5:
-    if __name__ == '__main__':
-        target = input('Enter the host to be scanned: ')
-        first_Port = int(input("What port would you like to start with? "))
-        last_Port = int(input("What is the ending port?"))
-        t_IP = gethostbyname(target)
-        print ('Starting scan on host: ', t_IP)
 
-        for i in range(first_Port, last_Port):
-            s = socket(AF_INET, SOCK_STREAM)
+if option == 1:
+    ping()
 
-            conn = s.connect_ex((t_IP, i))
+if option == 2:
+    hydra()
 
-            if(conn == 0):
-                print ('Port %d: OPEN' % (i,))
+if option == 3:
+    gobuster()
 
-            s.close()
+if option == 4:
+    john()
 
-        print("Scan finished")
+if option == 5:
+    burp()
 
-
-restart=str(input("Would you like to try another option? "))
-
-for x in yes_or_no:
-    print(x)
-
-if restart == "yes":
-    print("You selected yes")
-    for tool in tools:
-        print(tool)
-
+if option == 6:
+    portscanner()
+ 
